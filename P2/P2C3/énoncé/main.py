bc1a3add9e5ff749e211d3976ad6ec44547579b3
Delporte <maxime.delporte04@gmail.com>
2024-11-30 11:07:44 +0100

    P2C3

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,1 +1,18 @@
 # Ecrivez votre code ici
+def salaire_mensuel(salaire_annuel):
+    return salaire_annuel / 12
+
+def salaire_hebdomadaire(salaire_mensuel):
+    return salaire_mensuel / 4
+
+def salaire_horaire(salaire_hebdomadaire, heures_travaillees):
+    return salaire_hebdomadaire / heures_travaillees
+
+annuel = float(input("Indiquer votre salaire annuel: "))
+travaillees = float(input("Indiquer le nombre d'heure travailler: "))
+
+mensuel = salaire_mensuel(annuel)
+hebdomadaire = salaire_hebdomadaire(mensuel)
+horaire = salaire_horaire(hebdomadaire, travaillees)
+
+print(f"Votre salaire horaire est de {horaire} $")
